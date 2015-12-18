8075d7a39292ba6f7c14b3ed4f0d8c25dc2df9e0
John Schwartz Jacobsen <jsj@admins-MBP.lan>
2015-12-18 19:06:16 +0000

    workable starter template for angular2-meteor

---
+++ client/footer/footer.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../typings/angular2-meteor.d.ts" />
+
+import {Component, View} from 'angular2/core';
+
+@Component({
+	selector:'app-footer'
+})
+@View({
+	templateUrl:'client/footer/footer.html'
+})
+
+export class Footer{}
--- client/home/home.ts
+++ client/home/home.ts
@@ -4,13 +4,19 @@ import {Component, View} from 'angular2/core';
 import {TopNavigation} from 'client/navigation/top/top-navigation';
 import {Splash} from 'client/splash/splash';
 import {Featured} from 'client/featured/featured';
+import {Footer} from 'client/footer/footer';
 
 @Component({
 	selector:'home'
 })
 @View({
 	templateUrl:'client/home/home.html',
-	directives: [TopNavigation, Splash, Featured]
+	directives: [
+		TopNavigation,
+		Splash,
+		Featured,
+		Footer
+	]
 })
 
 export class Home{}
